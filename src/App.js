7d696147b51cee811f5bab96d502071089be981f
Sergey <62606319+Dan-Ser-Vol@users.noreply.github.com>
2023-03-31 18:31:27 +0300

    preview 3-5

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import {useForm} from "react-hook-form";
+import {savePost} from "./service/allService.";
 
 function App() {
   //
   // let [formValue, setFormValue] = useState({login:'', password:''})
 
 
-  const onsubmit = (data) => {
-    console.log(data)
+  const onsubmit = (post) => {
+  savePost(post).then(({data}) => console.log(data));
   }
 
   const {register, handleSubmit} = useForm()
 
-
   return (
       <div>
         <h1>APP COMPONENT</h1>
-
         <form onSubmit={handleSubmit(onsubmit)}>
           <input{...register('title', {required: true})} /> <br/>
           <input {...register('body')}/>
+          <select {...register('userId')}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
           <button>submit</button>
         </form>
 
--- src/service/allService..js
+++ src/service/allService..js
@@ -2,7 +2,9 @@ import axios from 'axios'
 
 let axiosInstance = axios.create({
   baseURL: 'https://jsonplaceholder.typicode.com',
-  headers: {}
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
 })
 
 const getUsers = () => {
@@ -13,6 +15,10 @@ const getUser = (id) => {
   return axiosInstance.get('/users/' + id)
 }
 
+const savePost = (data) => {
+  return axiosInstance.post('/posts', {data} )
+}
+
 export {
-  getUser, getUsers
+  getUser, getUsers, savePost
 }
