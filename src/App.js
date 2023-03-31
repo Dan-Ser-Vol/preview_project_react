336e7d1f2759ab3cf1efc555ba659724cadac7bc
Sergey <62606319+Dan-Ser-Vol@users.noreply.github.com>
2023-03-31 17:21:20 +0300

    preview 3-3

--- src/App.js
+++ src/App.js
@@ -1,52 +1,27 @@
-import {useEffect, useState} from "react";
-import UserComponent from "./components/UserComponent";
-import {getUser, getUsers} from "./service/userService.";
-import UserDetail from "./components/UserDetail";
+import {useForm} from "react-hook-form";
 
 function App() {
-  let [users, setUsers] = useState([])
-  let [user, setUser] = useState(null)
-  let [formValue, setFormValue] = useState({login:'', password:''})
+  //
+  // let [formValue, setFormValue] = useState({login:'', password:''})
 
 
-  useEffect(() => {
-    getUsers().then(res => setUsers([...res.data]))
-  }, [])
-
-  const chooseUser = (id) => {
-    getUser(id).then(res => setUser(res.data))
+  const onsubmit = (data) => {
+    console.log(data)
   }
 
-  const onsubmit = (e) => {
-    e.preventDefault()
-  }
+  const {register, handleSubmit} = useForm()
 
-  const onChange = (e) => {
-    setFormValue({...formValue , [e.target.name]:e.target.value})
-  }
 
   return (
       <div>
         <h1>APP COMPONENT</h1>
-        <form onSubmit={onsubmit}>
-          <input type="text" name={'login'} onChange={onChange} value={formValue.login}/> <br/>
-          <input type="text" name={'password'} onChange={onChange} value={formValue.password}/>
+
+        <form onSubmit={handleSubmit(onsubmit)}>
+          <input{...register('title', {required: true})} /> <br/>
+          <input {...register('body')}/>
           <button>submit</button>
         </form>
-        <h3>{formValue.login}</h3>
-        <h3>{formValue.password}</h3>
-        <h2>User Detail</h2>
-        {
-            user && <UserDetail user={user}/>
-        }
-        <h2>Users list</h2>
-        {
-          users.map(user => <UserComponent
-              key={user.id}
-              user={user}
-              chooseUser={chooseUser}
-          />)
-        }
+
       </div>
   );
 }
