12b35c793e8e7860dc98fea674c4d6cc6dc616e2
Sergey <62606319+Dan-Ser-Vol@users.noreply.github.com>
2023-03-31 16:46:54 +0300

    preview 3-1

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import UserDetail from "./components/UserDetail";
 function App() {
   let [users, setUsers] = useState([])
   let [user, setUser] = useState(null)
+  let [formValue, setFormValue] = useState({login:'', password:''})
+
 
   useEffect(() => {
     getUsers().then(res => setUsers([...res.data]))
@@ -15,12 +17,27 @@ function App() {
     getUser(id).then(res => setUser(res.data))
   }
 
+  const onsubmit = (e) => {
+    e.preventDefault()
+  }
+
+  const onChange = (e) => {
+    setFormValue({...formValue , [e.target.name]:e.target.value})
+  }
+
   return (
       <div>
         <h1>APP COMPONENT</h1>
+        <form onSubmit={onsubmit}>
+          <input type="text" name={'login'} onChange={onChange} value={formValue.login}/> <br/>
+          <input type="text" name={'password'} onChange={onChange} value={formValue.password}/>
+          <button>submit</button>
+        </form>
+        <h3>{formValue.login}</h3>
+        <h3>{formValue.password}</h3>
         <h2>User Detail</h2>
         {
-          user && <UserDetail user={user}/>
+            user && <UserDetail user={user}/>
         }
         <h2>Users list</h2>
         {
